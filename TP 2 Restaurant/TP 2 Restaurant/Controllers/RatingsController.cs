fea564332590f3d7f967987a29c3cae10ee1dfe2
ced210 <37204120+ced210@users.noreply.github.com>
2018-04-15 15:28:44 -0400

    Presque final

--- TP 2 Restaurant/TP 2 Restaurant/Controllers/RatingsController.cs
+++ TP 2 Restaurant/TP 2 Restaurant/Controllers/RatingsController.cs
@@ -9,6 +9,46 @@ namespace TP_2_Restaurant.Controllers
 {
     public class RatingsController : Controller
     {
+        #region Rating Sort
+
+        ///// <summary>
+        ///// Ced 15 Av
+        ///// 
+        ///// Initialise le tri par Date
+        ///// et en ordre décroissant
+        ///// </summary>
+        //private void InitializeRatingSessionSort()
+        //{
+        //    if (Session["RatingSortBy"] == null)
+        //    {
+        //        //Par défault,
+        //        // Tri par date
+        //        Session["RatingSortBy"] = "Date";
+        //        // Du plus Récent
+        //        Session["RatingSortAscendant"] = false;
+        //    }
+        //}
+
+        ///// <summary>
+        ///// Ced 15 av
+        ///// </summary>
+        ///// <param name="by"></param>
+        ///// <returns></returns>
+        //public ActionResult Sort(string by)
+        //{
+        //    if (by == (string)Session["RatingSortAscendant"])
+        //        Session["RatingSortAscendant"] = !(bool)Session["RatingSortAscendant"];
+        //    else
+        //        Session["RatingSortAscendant"] = true;
+        //    Session["RatingSortBy"] = by;
+        //    return View("allo");
+        //}
+        #endregion
+
+
+
+
+
         // GET: Ratings
         public ActionResult Index()
         {
@@ -21,8 +61,17 @@ namespace TP_2_Restaurant.Controllers
         /// <returns></returns>
         public ActionResult Create()
         {
+            //ced 15 av
+            //Pour En-tete du resto courrant
+            Restaurant resto = null;
+
             using (var DB = new RestaurantsEntities())
             {
+                //ced 15 av
+                //Pour En-tete du resto courrant
+                resto = (Restaurant)Session["CurrentRestaurant"];
+                ViewBag.RestaurantViews = resto.ToRestaurantView();
+
                 RatingView ratingView = new RatingView();
                 ratingView.Restaurant_Id = ((Restaurant)Session["CurrentRestaurant"]).Id;
                 return View(ratingView);
@@ -45,6 +94,8 @@ namespace TP_2_Restaurant.Controllers
                     //le resto courrant de la page Details d'avant
                     rating.Restaurant_Id = ((Restaurant)Session["CurrentRestaurant"]).Id;
 
+  
+
                     DB.Create(rating);
                     return RedirectToAction("Details", "Restaurants", new {id = ((Restaurant)Session["CurrentRestaurant"]).Id });
                 }
@@ -52,6 +103,26 @@ namespace TP_2_Restaurant.Controllers
                 ViewBag.RestaurantToEvaluate = ((Restaurant)Session["CurrentRestaurant"]).ToRestaurantView();
                 return View(ratingview);
             }
+        }
+        /// <summary>
+        /// Dom 15 av
+        /// </summary>
+        /// <param name="id">Id du Rating</param>
+        /// <returns></returns>
+        public ActionResult Delete(int id)
+        {
+            int idResto;
+            using (var DB = new RestaurantsEntities())
+            {
+                Rating rate = DB.Ratings.Find(id);
+                idResto = rate.Restaurant_Id;
+                if (rate != null)
+                {
+                    DB.DeleteRating(id);
+                }
+            }
+            return RedirectToAction("Details/", "Restaurants", new { id = ((Restaurant)Session["CurrentRestaurant"]).Id });
+            //return RedirectToAction("Details/" + idResto);
         }
     }
 }
